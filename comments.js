f0fe24f82b2376c0db81456fd09e9755712b3578
juanlecho <42351148+juanlecho@users.noreply.github.com>
2023-07-17 21:08:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,58 +1,29 @@
-// Create new web server to listen on port 3000
+// Create new web server
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { randomBytes } = require('crypto');
-const axios = require('axios');
-// Create a new express application
 const app = express();
-// Add middleware
-app.use(bodyParser.json());
-app.use(cors());
-// Create an empty object to store comments
-const commentsByPostId = {};
-// Create a route handler for GET requests to /posts/:id/comments
-app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
+// Import comments data
+const comments = require('./data/comments');
+
+// Set up server port
+const port = 3000;
+
+// Set up route for comments data
+app.get('/comments', (req, res) => {
+    res.json(comments);
 });
-// Create a route handler for POST requests to /posts/:id/comments
-app.post('/posts/:id/comments', async (req, res) => {
-    // Generate a random id for the comment
-    const commentId = randomBytes(4).toString('hex');
-    // Get the content from the request body
-    const { content } = req.body;
-    // Get the comments array for the post id
-    const comments = commentsByPostId[req.params.id] || [];
-    // Add the new comment to the comments array
-    comments.push({ id: commentId, content, status: 'pending' });
-    // Update the comments array for the post id
-    commentsByPostId[req.params.id] = comments;
-    // Send a response to the user
-    await axios.post('http://localhost:4005/events', {
-        type: 'CommentCreated',
-        data: {
-            id: commentId,
-            content,
-            postId: req.params.id,
-            status: 'pending'
-        }
-    });
-    res.status(201).send(comments);
+
+// Set up route for comments data by id
+app.get('/comments/:id', (req, res) => {
+    // Find comment by id
+    const found = comments.some(comment => comment.id === parseInt(req.params.id));
+    if (found) {
+        res.json(comments.filter(comment => comment.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({ msg: `No comment with the id of ${req.params.id}` });
+    }
 });
-// Create a route handler for POST requests to /events
-app.post('/events', async (req, res) => {
-    console.log('Received Event', req.body.type);
-    const { type, data } = req.body;
-    if (type === 'CommentModerated') {
-        const { id, postId, status, content } = data;
-        const comments = commentsByPostId[postId];
-        const comment = comments.find(comment => {
-            return comment.id === id;
-        });
-        comment.status = status;
-        await axios.post('http://localhost:4005/events', {
-            type: 'CommentUpdated',
-            data: {
-                id,
-                postId,
-                status,
+
+// Set up server port to listen for request
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
